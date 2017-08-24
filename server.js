1dcecfb24343ebdf7e8dc7575c64119bdf685ac7
skirubame <skirubame@gmail.com>
2017-08-24 19:11:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,19 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/article-one',function(req,res) {
+    res.send("This is article-one");
+});
+
+app.get('/article-two',function(req,res) {
+    res.send("This is article-two");
+});
+
+app.get('/article-three',function(req,res) {
+    res.send("This is article-three");
+});
+
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
