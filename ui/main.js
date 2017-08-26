a5e012d9560f891da19bb07dc3be9e468d55df23
skirubame <skirubame@gmail.com>
2017-08-26 15:31:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //change the inner text
 var element=document.getElementById("main-text");
 element.innerHTML="newvalue";
+var image=document.getElementById("madi");
+
+image.onclick fuction() {
+    image.style.marginleft='100px';
+};
