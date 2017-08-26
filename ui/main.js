e1b330bda04ded60eac8c08d9e4216a4b48fed82
skirubame <skirubame@gmail.com>
2017-08-26 22:21:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,7 @@
-console.log('Loaded!');
-//change the inner text
-var element=document.getElementById("main-text");
-element.innerHTML="newvalue";
-
-var img=document.getElementById("madi");
-var marginLeft=0;
-
- function moveRight (){
-marginLeft=marginLeft+1;
-img.style.marginLeft=marginLeft+"px";
-     
- }
-img.onclick=function() {
-  img.style.marginLeft = "100px" ;
-var interval=setInterval(moveRight,100);
-    
+//counter
+var button=document.getElementById('counter');
+button.onclick= function() {
+  counter=counter+1;
+  var span=document.getElementById('count');
+  span.innerHTML=counter.toString();
 };
