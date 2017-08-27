bd7e9ddf21a1e1abd0f0cb47cbb03c445785038d
skirubame <skirubame@gmail.com>
2017-08-27 12:21:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,19 @@
 var button=document.getElementById('counter');
 counter=0;
 button.onclick= function() {
-  counter=counter+1;
+    var request=new XMLHttpRequest();
+    request.onreadystatechange= fuction();
+    if(request.readyState==XMLHttpRequest.Done)
+    {
+        if(request.status==200)
+        {
+        var counter=request.responseText;
   var span=document.getElementById('count');
   span.innerHTML=counter.toString();
+        }
+    }
+    
+  request.open('Get','http://kirubakarans.imad.hasura-app.io/counter',true);
+  request.send(null); 
+ 
 };
