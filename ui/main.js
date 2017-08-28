39fd042b6f761eed1627238468d21e2e891cbcc2
skirubame <skirubame@gmail.com>
2017-08-28 21:42:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,15 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function() {
-var names=['name1','name2','name3','name4'];
+     var request=new XMLHttpRequest();
+
+    request.onreadystatechange = function()     {
+    if(request.readyState === XMLHttpRequest.DONE) {
+        if(request.status ===  200)
+        {
+        
+    var names=request.responseText;
+    names=JSON.parse(names);//covert to array
 var list='';
 for(var i=0;i<names.length;i++) {
     list+='<li>'+names[i]+'</li>';
@@ -32,5 +40,12 @@ for(var i=0;i<names.length;i++) {
 }
  var ul=document.getElementById('namelist');
  ul.innerHTML=list;
+    }
+    }
+    
+    };
+  request.open('GET','http://kirubakarans.imad.hasura-app.io/submit-name='+name,true);
+  request.send(null); 
+    
 };
 
