78e03b9fbdaaaed767cd38231d3227e3f9fd7202
skirubame <skirubame@gmail.com>
2017-08-28 20:23:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,8 +26,8 @@ var submit=document.getElementById('submit_btn');
 submit.onclick=function() {
 var names=['name1','name2','name3','name4'];
 var list='';
-for(var i=0;i<name.length;i++) {
-    list+='<li>'+name[i]+'</li>';
+for(var i=0;i<names.length;i++) {
+    list+='<li>'+names[i]+'</li>';
     
 }
  var ul=document.getElementById('namelist');
