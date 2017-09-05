8b8832632cae9aa901fe16656766db523f60643c
skirubame <skirubame@gmail.com>
2017-09-05 19:53:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -138,8 +138,21 @@ app.get('/submit-name',function(req,res) {//url://submit-name?name=xxxx
    
    res.send(JSON.stringify(names));
 });
-app.get('/:articlename', function(req,res) {
-    var article=req.params.articlename;
+app.get('/artciles/:articlename', function(req,res) {
+    
+    pool.query("SELECT * FROM article WHERE title="+ req.params.articlename,function(err,result) {
+    if(err) {
+        res.status(500).send(err.toString());
+    }else{
+        if(result.rows.length===0){
+        res.status(404).send('article not found');
+        }else
+    {
+     var articleData=result.rows[0];
+     res.send(createTemplate(articleData));
+    }    
+    }
+}); 
     res.send(createtemplate(articles[article]));
 });
 
