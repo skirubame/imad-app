dfc980d1c8b8c789a9459a81523514ed58f17d75
skirubame <skirubame@gmail.com>
2017-09-05 20:18:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -146,8 +146,7 @@ app.get('/articles/:articlename', function(req,res) {
     }else{
         if(result.rows.length===0){
         res.status(404).send('article not found');
-        }else
-    {
+        }else {
      var articleData=result.rows[0];
      res.send(createTemplate(articleData));
     }    
