3b6dda4302b879ea88784e2b177e634a0d807887
skirubame <skirubame@gmail.com>
2017-09-07 20:53:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,6 +104,34 @@ app.post('/create-user',function(req,res){
     }
 });    
 });
+app.post('/login', function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+    
+    pool.query('SELECT *from INTO "user" username=$1',[username],function(err,result){
+        
+    if(err) {
+        res.status(500).send(err.toString());
+    }else{
+        if(result.rows.length===0) {
+        res.send(403).send('user invalid');
+    }
+        else{
+            var dbString=result.rows[0].password;
+            var salt=dbString.split('$')[2];
+            var hashPassword=hash(password,salt);
+            if(hashPassword===dbString) {
+                res.send('credential correct');
+            } else{
+                res.send(403).send("credential mismatch");
+            }
+        }
+    }
+}); 
+    
+    
+    
+});
 
 function createtemplate(data){
  title=data.title;
