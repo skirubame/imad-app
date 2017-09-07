f782f34c7d78d7dd5fb631607165391fb981efe1
skirubame <skirubame@gmail.com>
2017-09-07 20:42:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.post('/create-user',function(req,res){
     var username=req.body.username;
     var password=req.body.password;
     var salt=crypto.randomBytes(128).toString('hex');
-    var doString=hash(password,salt);
+    var dbString=hash(password,salt);
     pool.query('INSERT INTO "user"(username,password) VALUES ($1, $2)',[username,dbString],function(err,result){
         
     if(err) {
